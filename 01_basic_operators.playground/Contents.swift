db1acd7a654e60b95627a8e32e79c74736ebdbc5
노용훈 <mogi3206@noyonghunui-MacBook-Pro.local>
2017-07-13 16:47:51 +0900

    Add basic operators

---
+++ 01_basic_operators.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//Str
+
+// variable 변수
+var number1 = 1
+number1 = 3
+number1 += 5
+number1 -= 1
+number1 *= 2
+
+
+// 상수(변하지 않는 수. 변경하려면 에러 발생)
+let number2 = 2
+
+
+1 + 2
+1+2
+5-3
+2*3
+10.0 / 2.5
+
+5.0/2.0
+
+5 % 2
+5 % 3
+5 % 4
+5%5
+
+9 % 4
+9 % 3
+9 % 2
+9 % 1
+9 % 5
+9 % 6
+
+
+
+"abc"+"def"
